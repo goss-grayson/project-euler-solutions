7c472e0afd1532e1cf83bd362125d46d6f2fa18c
goss-grayson <60072758+goss-grayson@users.noreply.github.com>
2020-02-22 04:23:52 -0600

    Update Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -2,15 +2,13 @@
 ### 01/02/2020
 ### Euler Problem 1 Solution
 
-summation = 0;
+s = 0;
 
-for i in range(3, 1000):
+for i in range(3, 1001):
     if ( ( i % 3 ) == 0 ):
-        summation = summation + i;
-        continue;
-    if ( ( ( i % 5 ) == 0 ) and ( (i % 15) != 0 ) ):
-        summation = summation + i;
-        continue;
+        s += i;
+    elif ( ( ( i % 5 ) == 0 ) and ( ( i % 15 ) != 0 ) ):
+        s += i;
     
 print(summation);
 
